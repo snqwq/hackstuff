6b8090cad48a0da8c3a1f051f5633f765f016ba3
snyeg <99054951+snqwq@users.noreply.github.com>
2025-09-09 12:53:43 +0000

    revert to 1 col on mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -92,7 +92,7 @@ export default function Home(): JSX.Element {
         <p className="text-xl text-gray-600">guhhhh!?!??!</p>
       </header>
 
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
         {items.map((item) => (
           <ItemCard key={item.id} item={item} />
         ))}
