145cea312eb773cebe150780366343378413e78b
snyeg <99054951+snqwq@users.noreply.github.com>
2025-09-10 17:29:31 +0000

    opengraph tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,10 +4,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta property="og:title" content="HackStuff"></meta>
+        <meta property="og:description" content="uhhh add description later"></meta>
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet"
         ></link>
+
       </Head>
       <body className="antialiased">
         <Main />
